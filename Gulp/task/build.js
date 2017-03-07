011b025dbd2d6ca8913e633c64e84b4bbc714ad5
Viachaslau Lyskouski <s.lyskouski@creativity.by>
2017-03-07 23:16:23 +0300

    Gulp tasks

--- Gulp/task/build.js
+++ Gulp/task/build.js
@@ -6,12 +6,30 @@
 
 // Gulp initialization
 var gulp = require('gulp');
+var change = require('gulp-change');
 var runSequence = require('run-sequence');
 
 gulp.task('build', function (callback) {
+    var sources = require('./params/minify.json');
+
+    // Update version and timestamp
+    console.log('* updating timestamp');
+    var jsonPath = './config/task/params/';
+    gulp.src(jsonPath + 'minify.json')
+        .pipe(change(function () {
+            for (var i in sources) {
+                sources[i].datetime = (new Date).getTime();
+            }
+            return JSON.stringify(sources, {}, "    ");
+        }))
+        .pipe(gulp.dest(jsonPath));
+
+    // Build release (@note: `css`, `js` cannot be triggered in parallel)
     runSequence(
+        'sass',
         'css',
         'js',
+        'image',
         callback
     );
     console.log('Build compiled!');
