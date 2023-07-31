70cea5d08f2328a6f0fcd791370b2cf364c0a461
Viachaslau Lyskouski <viachaslau.lyskouski@globallogic.com>
2023-07-31 21:44:36 +0200

    Score Board [JS]. Fix single remove statement

--- JavaScript/score-board/lib/storage.js
+++ JavaScript/score-board/lib/storage.js
@@ -3,7 +3,7 @@ const cache = require("memory-cache");
 const storage = {
     timeout: 60 * 60 * 1000,
     get: (key) => cache.get(key),
-    delete: (key) => cache.clear(key),
+    delete: (key) => cache.del(key),
     set: (key, value) => cache.put(key, value, storage.timeout)
 };
 
